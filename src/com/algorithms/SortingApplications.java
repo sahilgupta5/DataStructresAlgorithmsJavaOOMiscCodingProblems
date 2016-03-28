c9e97963636c6f9f4e0f6bab6412af27923248a1
sahilgupta5 <sahilgupta5@hotmail.com>
2016-03-27 20:51:45 -0400

    Adding a method to sorting applications

--- src/com/algorithms/SortingApplications.java
+++ src/com/algorithms/SortingApplications.java
@@ -1,4 +1,5 @@
 package com.algorithms;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -12,6 +13,13 @@ public class SortingApplications {
 		return new String(c);
 	}
 
+	/**
+	 * Write a method to sort an array of strings so that all the anagrams are
+	 * next to each other.
+	 * 
+	 * @param inArr
+	 * @return
+	 */
 	public static String[] getSortedAnagramHash(String[] inArr) {
 
 		HashMap<String, LinkedList<String>> m = new HashMap<String, LinkedList<String>>();
@@ -36,7 +44,30 @@ public class SortingApplications {
 		return inArr;
 	}
 
-	public static void main(String args[]) {
+	/**
+	 * Problem: Given a sorted array of n integers that has been rotated an
+	 * unknown number of times, write code to find an element in the array. You
+	 * may assume that the array was originally sorted in increasing order.
+	 * 
+	 * Solution: Perform a binary search to find the element. Since, it is in
+	 * sorted order mostly we can find the number in O(log(n)) time.
+	 * 
+	 * @param args
+	 */
+
+	public int getElementIndexSortedArray(int[] arr, int numFind) {
+
+		int index = arr.length / 2;
+
+		for (int i = 0; i < arr.length; i++) {
+
+		}
+
+		return index;
+
+	}
+
+	public void testCase1SortedAnagramHash() {
 		String[] arr = { "abd", "dab", "god", "bad", "dog" };
 		// String[] arr1 = null;
 
@@ -45,6 +76,19 @@ public class SortingApplications {
 			System.out.println(s);
 		}
 		// System.out.println(getSortedAnagramHash(arr1).toString());
+	}
+
+	public void testCase2ElementIndexSortedArray() {
+		int[] arr = { 56, 69, 73, 78, 81, 3, 5, 8, 13, 21, 25, 45 };
+		int num = arr[6];
+		System.out.println(getElementIndexSortedArray(arr, num));
+	}
+
+	public static void main(String args[]) {
+
+		SortingApplications sa = new SortingApplications();
+		sa.testCase1SortedAnagramHash();
+		sa.testCase2ElementIndexSortedArray();
 	}
 
 }
