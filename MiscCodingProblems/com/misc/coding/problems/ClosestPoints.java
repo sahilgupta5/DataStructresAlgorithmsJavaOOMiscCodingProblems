c11ec90815957b2a259ee45cc4db36eeee2cc7e7
sahilgupta5 <sahilgupta5@hotmail.com>
2016-03-28 00:04:58 -0400

    Implement finding closest points to a point in a graph (adhip's question to me)

---
+++ MiscCodingProblems/com/misc/coding/problems/ClosestPoints.java
@@ -0,0 +1,16 @@
+package com.misc.coding.problems;
+
+public class ClosestPoints {
+
+	/**
+	 * Given the xy axis, We have the origin (0,0) and multiple points in the
+	 * plane. Can you give me the top 100 closest elements.
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String args[]) {
+
+	}
+
+}
